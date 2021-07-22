574ded0cdb77e57eb64c55eb0cace0e17e7ad728
Sam <earthstamper@gmail.com>
2021-07-22 16:49:47 +0200

    rc1 avior-dis

--- src/dir.rs
+++ src/dir.rs
@@ -1,79 +1,142 @@
-use avior_infuser_lib::log::Logger;
+use avior_infuser_lib::log::Log;
+use avior_infuser_lib::Job;
+use avior_infuser_lib::{log::Logger, AssignedClient};
 
-use crate::{LogExt, cfg::Config};
+use crate::{cfg::Config};
 
+use std::convert::TryFrom;
+use std::time::SystemTime;
 use std::{
-    path::Path,
     error::Error,
-    fs::{self, DirEntry, File},
-    io::{self, BufReader, prelude::*}
+    fs::{self, File},
+    io::{prelude::*, BufReader},
+    path::Path,
 };
-
 
 pub struct DirectoryTraverser<'yingking> {
     cfg: &'yingking Config,
-    logger: &'yingking mut Logger
+    logger: &'yingking mut Logger,
 }
 
 impl<'yingking> DirectoryTraverser<'yingking> {
-    pub fn new(cfg: &'yingking Config, logger: &'yingking mut Logger) -> DirectoryTraverser<'yingking> {
+    pub fn new(
+        cfg: &'yingking Config,
+        logger: &'yingking mut Logger,
+    ) -> DirectoryTraverser<'yingking> {
         DirectoryTraverser { cfg, logger }
     }
 
-    pub fn traverse(&mut self, dir: &str) -> Result<(), Box<dyn Error>> {
+    pub fn traverse(&mut self, dir: &str) -> Result<Vec<Job>, Box<dyn Error>> {
+        let mut jobs: Vec<Job> = Vec::new();
         for entry in fs::read_dir(Path::new(dir))? {
             let file = entry?;
             if !file.file_type()?.is_file() {
-              continue;
+                continue;
             }
             let path = file.path();
             let extension = path
                 .extension()
                 .and_then(|osstr| osstr.to_str())
                 .unwrap_or("");
-            let filetypes_iterator = self.cfg.filetypes.iter();
-            if filetypes_iterator.any(|filetype| filetype == extension) {
-                let filename = path.file_name()
-                .and_then(|osstr| osstr.to_str())
-                .unwrap_or("");
-                let ignored_filetypes_iter = self.cfg.ignored_filetypes.iter();
+
+            // only iterate over files that have the specified file extension
+            let mut filetypes_iterator = self.cfg.filetypes.iter();
+            if filetypes_iterator.any(|filetype| filetype == &extension.to_lowercase()) {
+                let filename = path
+                    .file_name()
+                    .and_then(|osstr| osstr.to_str())
+                    .unwrap_or("");
+
+                let mut ignored_filetypes_iter = self.cfg.ignored_filetypes.iter();
+
+                // filter out ignored files
                 if ignored_filetypes_iter.any(|suffix| filename.ends_with(suffix)) {
                     continue;
-                } else {
-                  let file_stem = path.file_stem().and_then(|osstr| osstr.to_str()).unwrap_or("").to_owned();
-                  file_stem.push_str(".txt");
-                  let (name, subtitle) = get_file_titles(&file_stem, self.logger)?;
+                }
+
+                let path_to_str = match path.to_str() {
+                    Some(path_str) => path_str,
+                    None => {
+                        self.logger.add(&"could not find path string");
+                        continue;
+                    }
+                };
+
+                // filter out files that are too new
+                if let Ok(metadata) = file.metadata() {
+                    let last_modified = metadata.modified()?;
+                    let duration = match SystemTime::now().duration_since(last_modified) {
+                        Ok(duration) => duration,
+                        Err(_) => continue
+                    };
+                    let days_duration = i32::try_from(duration.as_secs() / (60 * 60 * 24)).unwrap_or(0);
+                    if days_duration < self.cfg.min_age {
+                        println!("skipping {} due to ({} age / {} mininum)", path_to_str, days_duration, self.cfg.min_age);
+                        continue;
+                    }
+                }
+
+                println!("scanning {}", path_to_str);
+
+                let parse_result =
+                    self.get_file_titles(&path.with_extension("txt").to_str().unwrap_or_default());
+
+                // if found, add job to the output vector
+                if let Ok((name, subtitle)) = parse_result {
+                    let job = Job {
+                        id: None,
+                        name,
+                        subtitle,
+                        path: String::from(path_to_str),
+                        assigned_client: AssignedClient::default(),
+                        custom_parameters: Vec::new(),
+                    };
+                    jobs.push(job);
                 }
             }
         }
-        Ok(())
+        Ok(jobs)
+    }
+
+    fn get_file_titles(&mut self, path: &str) -> Result<(String, String), Box<dyn Error>> {
+        let mut name: String = "".to_string();
+        let mut subtitle: String = "".to_string();
+
+        let file = match File::open(Path::new(path)) {
+            Ok(opened_file) => opened_file,
+            Err(err) => {
+                self.logger.add(&format!("{} for: {}", err, path));
+                return Err(Box::new(err));
+            }
+        };
+        //let decoded = DecodeReaderBytesBuilder::new().encoding(Some(WINDOWS_1252)).build(file);
+        let reader = BufReader::new(file);
+
+        for line in reader.lines() {
+            let line = match line {
+                Ok(line) => line,
+                Err(err) => {
+                    self.logger.add(&format!("{} for: {}", err, path));
+                    return Err(Box::new(err));
+                }
+            };
+            let name_tag = "Title=";
+            let subtitle_tag = "Info=";
+            if line.starts_with(name_tag) {
+                name = line[name_tag.len()..].to_owned();
+            } else if line.starts_with(subtitle_tag) {
+                subtitle = line[subtitle_tag.len()..].to_owned();
+            }
+        }
+        Ok((name.to_owned(), subtitle.to_owned()))
     }
 }
-
-fn get_file_titles(path: &str, logger: &mut Logger) -> Result<(String, String), Box<dyn Error>> {
-    let mut name: String;
-    let mut subtitle: String; 
-    let file = File::open(Path::new(path)).log(logger)?;
-    let file = BufReader::new(file);
-    for line in file.lines() {
-      let line = line?;
-      let name_tag = "Title=";
-      let subtitle_tag = "Info=";
-      if line.starts_with(name_tag) {
-        name = line[name_tag.len()-1..].to_owned();
-      } else if line.starts_with(subtitle_tag) {
-        subtitle = line[subtitle_tag.len()-1..].to_owned();
-      }
-    }
-    Ok((name.to_owned(), subtitle.to_owned()))
-  }
 
 //directory
 // media file.(mkv/mpg/ts/whatevs)
 // media file.log - ignore
 // media file.txt - ignore
 // media file.INFO.log => no JOB
-
 
 /*
 [General]
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-use std::{
-    collections::{BTreeMap, HashMap},
-    error::Error,
-};
+use core::panic;
+use std::{collections::{BTreeMap, HashMap}, env, error::Error, fmt::Display};
 mod cfg;
 mod dir;
-use avior_infuser_lib::log;
 use avior_infuser_lib::MongoClient;
 use avior_infuser_lib::*;
 use log::{Log, Logger};
+
+use crate::dir::DirectoryTraverser;
 static CFG_STRING: &str = "dis_config.toml";
 static LOG_PATH: &str = "avior_dis.log";
 const IDENTITY: &str = "avior dis, version 0.1.0 - tarantula";
 
 trait LogExt {
+    fn log_and_flush(self, logger: &mut Logger) -> Self;
     fn log(self, logger: &mut Logger) -> Self;
 }
 
@@ -20,7 +20,7 @@ impl<T, E> LogExt for Result<T, E>
 where
     E: std::fmt::Display + std::fmt::Debug,
 {
-    fn log(self, logger: &mut Logger) -> Self {
+    fn log_and_flush(self, logger: &mut Logger) -> Self {
         if let Err(e) = &self {
             logger.add(&format!("{}", &e));
             if let Err(e) = logger.flush(LOG_PATH, log::Mode::Append) {
@@ -29,14 +29,54 @@ where
         }
         self
     }
+    fn log(self, logger: &mut Logger) -> Self {
+        if let Err(e) = &self {
+            logger.add(&format!("{}", &e));
+            eprint!("{:?}", e);
+        }
+        self
+    }
 }
 
+#[derive(Debug)]
+struct VecWrapper<'a>(&'a [String]);
+
+impl<'a> Display for VecWrapper<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut out = String::new();
+        out.push('[');
+        for (idx, elem) in self.0.iter().enumerate() {
+            out.push_str(elem);
+            if idx < self.0.len() - 1 {
+                out.push_str(", ")
+            } else {
+                out.push(']');
+            }
+        }
+        write!(f, "{}", out)
+    }
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut logger: Logger = Log::new(IDENTITY);
+
+    let args: Vec<String> = env::args().collect();
+    // print args using println! except for the first one
+    println!("cli arguments:");
+    for (idx, arg) in args.iter().enumerate() {
+        if idx > 0 {
+            println!("{}", arg);
+        }
+    }
+    if args.len() < 2 {
+        panic!("invalid scan path, scan path must be specified");
+    }
+
+    logger.add(&format!("scan directory: {}", args[1]));
+
     let cfg = cfg::read(CFG_STRING)?;
     let db_client: MongoClient = db::connect(&cfg.db_url)?;
-    let client_vec = db::get_clients(&db_client, &cfg.db_name).log(&mut logger)?;
+    let client_vec = db::get_clients(&db_client, &cfg.db_name).log_and_flush(&mut logger)?;
     let default_client = client_vec
         .iter()
         .find(|client| client.name == cfg.default_client)
@@ -45,33 +85,12 @@ fn main() -> Result<(), Box<dyn Error>> {
         client_vec,
         db::get_machine_jobcount(&db_client, &cfg.db_name)?,
     );
-    
-    let j1 = Job {
-        id: None,
-        path: "\\\\vdr-u\\SDuRec\\Recording\\exists\\Geheimnisvolle Wildblumen_2021-04-10-14-58-01-arte HD (AC3,deu).ts".to_string(),
-        name: "Geheimnisvolle Wildblumen".to_string(),
-        subtitle: "Blütenpracht im Wald".to_string(),
-        assigned_client: AssignedClient::default(),
-        custom_parameters: Vec::new()
-    };
-    let j2 =  Job {
-        id: None,
-        path: "\\\\vdr-u\\SDuRec\\Recording\\Die Zauberflöte von Mozart_2021-04-18-23-48-00-arte HD (AC3,deu).ts".to_string(),
-        name: "Die Zauberflöte von Mozart".to_string(),
-        subtitle: "".to_string(),
-        assigned_client: AssignedClient::default(),
-        custom_parameters: Vec::new()
-    };
-    let j3 =  Job {
-        id: None,
-        path: "\\\\vdr-u\\SDuRec\\Recording\\Die Zauberflizzle von Mozizzle_2021-04-18-23-48-00-arte HD (AC3,deu).ts".to_string(),
-        name: "Die Zauberflizzle von Mozizzle".to_string(),
-        subtitle: "".to_string(),
-        assigned_client: AssignedClient::default(),
-        custom_parameters: Vec::new()
-    };
 
-    let parsed_jobs = vec![j1, j2];
+
+    let mut dir_trav: DirectoryTraverser = DirectoryTraverser::new(&cfg, &mut logger);
+
+    //let parsed_jobs = vec![j1, j2];
+    let parsed_jobs = dir_trav.traverse(&args[1]).log_and_flush(&mut logger)?;
     let res = push_all_parsed(
         parsed_jobs,
         &db_client,
@@ -81,7 +100,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         &mut logger,
     );
 
-    logger.flush(LOG_PATH, log::Mode::Append);
+    logger.flush(LOG_PATH, log::Mode::Append)?;
     res
 }
 
@@ -106,40 +125,41 @@ fn push_all_parsed(
                         "error {:?} while trying to find job {}",
                         err, &parsed_job.path
                     ));
-                    return false;
+                    return false
                 }
             };
         })
         .collect();
+    if insert_jobs.len() == 0 {
+        logger.add(&format!{"database already up to date"})
+    }
     for job in insert_jobs.iter_mut() {
         let grouped_clients_reload = grouped_clients.clone();
         let eligible = get_eligible_client(&grouped_clients_reload);
-        let push_result;
-        match eligible {
+        let push_result  = match eligible {
             // push to eligible if available
             Ok((client, current, max)) => {
-                push_result =
-                    push_and_increment(&db_client, db_name, job, &client, grouped_clients);
                 logger.add(&format!(
-                    "pushed {} to {} with {}/{} job(s) and priority {}",
+                    "pushing {} to {} with {}/{} job(s) and priority {}",
                     job.path, client.name, current, max, client.priority
                 ));
+                push_and_increment(&db_client, db_name, job, &client, grouped_clients)
             }
             // push to default instead if no client is eligible
             Err(_) => {
-                push_result = push_and_increment(
+                logger.add(&format!(
+                    "no eligible found, pushing {} to default client {}",
+                    job.path, default_client.name
+                ));
+                push_and_increment(
                     &db_client,
                     db_name,
                     job,
                     &default_client,
                     grouped_clients,
-                );
-                logger.add(&format!(
-                    "no eligible found, pushed {} to default client {}",
-                    job.path, default_client.name
-                ));
+                )
             }
-        }
+        };
         // if a db push fails, log it and retry with the next job.
         // In case the database is messed up, dis can be run again safely since it doesn't add duplicates
         if let Err(e) = push_result {
@@ -182,4 +202,58 @@ param1: path
 param2: 3
 
 => library
+
+    
+    let j1 = Job {
+        id: None,
+        path: "\\\\vdr-u\\SDuRec\\Recording\\exists\\Geheimnisvolle Wildblumen_2021-04-10-14-58-01-arte HD (AC3,deu).ts".to_string(),
+        name: "Geheimnisvolle Wildblumen".to_string(),
+        subtitle: "Blütenpracht im Wald".to_string(),
+        assigned_client: AssignedClient::default(),
+        custom_parameters: Vec::new()
+    };
+    let j2 =  Job {
+        id: None,
+        path: "\\\\vdr-u\\SDuRec\\Recording\\Die Zauberflöte von Mozart_2021-04-18-23-48-00-arte HD (AC3,deu).ts".to_string(),
+        name: "Die Zauberflöte von Mozart".to_string(),
+        subtitle: "".to_string(),
+        assigned_client: AssignedClient::default(),
+        custom_parameters: Vec::new()
+    };
+    let j3 =  Job {
+        id: None,
+        path: "\\\\vdr-u\\SDuRec\\Recording\\Die Zauberflizzle von Mozizzle_2021-04-18-23-48-00-arte HD (AC3,deu).ts".to_string(),
+        name: "Die Zauberflizzle von Mozizzle".to_string(),
+        subtitle: "".to_string(),
+        assigned_client: AssignedClient::default(),
+        custom_parameters: Vec::new()
+    };
+
 */
+
+
+
+#[cfg(test)]
+mod tests {
+    use std::error::Error;
+    use crate::cfg;
+    use crate::dir::DirectoryTraverser;
+    use avior_infuser_lib::log::*;
+
+    static IDENTITY: &str = "avior dis, version 0.1.0 - tarantula";
+    static CFG_STRING: &str = "dis_config.toml";
+    static LOG_PATH: &str = "avior_traverse_test.log";
+
+    #[test]
+    fn test_traverse() -> Result<(), Box<dyn Error>> {
+        let mut logger: Logger = Log::new(IDENTITY);
+        let cfg = cfg::read(CFG_STRING)?;
+        let mut dir_trav: DirectoryTraverser = DirectoryTraverser::new(&cfg, &mut logger);
+        let mut job_vector = dir_trav.traverse(&"D:\\Recording");
+        // print the vector
+        for job in job_vector.iter_mut() {
+            logger.add(&format!("{:?}", job));
+        }
+        logger.flush(LOG_PATH, Mode::Overwrite)
+    }
+}
